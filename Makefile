12f4349238c37235d63f7ea97af3ee7ada1b5f99
Yuhan Deng <yhdeng@stanford.edu>
2026-07-29 11:20:59 -0700

    fix: cleanup

--- Makefile
+++ Makefile
@@ -2,12 +2,14 @@
 
 all: thy
 	isabelle build -D wasm-proofs -v Wasm-Proof
-	
 
-wasm-proofs/Wasm-Proof/Init.thy: coupon.wat generate.sh
+wasm-proofs/Wasm-Proof/Init.thy: coupon.wat generate.sh interpreter
 	./generate.sh
 
 thy: wasm-proofs/Wasm-Proof/Init.thy
+
+interpreter:
+	$(MAKE) -C spec/interpreter
 
 
 
---
+++ scripts/opam-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+opam switch create 4.11.2                                                                                                                                                                                     
+opam install ocamlbuild -y                                                                                                                                                                                    
+opam install zarith -y
